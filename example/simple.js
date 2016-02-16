3b22b651eac12d4b9e9c43cc22ab404171e8b306
Diana Shkolnikov <dianashk@gmail.com>
2016-02-16 16:59:54 -0500

    Fix example and doc

--- example/simple.js
+++ example/simple.js
@@ -4,14 +4,18 @@ var sink = require('through2-sink');
 var maxInFlight = process.argv[2] || 1;
 var parallelStream = require('../index.js');
 
-var seeYouLaterStream = parallelStream(maxInFlight, function (doc, enc, next) {
-  console.log('I see you, ' + doc.name);
+var seeYouLaterStream = parallelStream(maxInFlight,
+  function (doc, enc, next) {
+    console.log('I see you, ' + doc.name);
 
-  setTimeout(function () {
-    doc.msg = 'Oh hey there again, ' + doc.name;
-    next(null, doc);
-  }, 1000);
-});
+    setTimeout(function () {
+      doc.msg = 'Oh hey there again, ' + doc.name;
+      next(null, doc);
+    }, 1000);
+  },
+  function () {
+    console.log('Ooh, looks like the stream is finished');
+  });
 
 streamArray([{name:'Diana'}, {name:'Julian'}, {name:'Stephen'}, {name:'Peter'}])
   .pipe(seeYouLaterStream)
